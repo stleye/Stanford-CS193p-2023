d04e94b07a1f853d5752f922039837200bd14f5e
Sebastian Tleye <stleye@gmail.com>
2023-12-17 10:27:29 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sebastian Tleye on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
